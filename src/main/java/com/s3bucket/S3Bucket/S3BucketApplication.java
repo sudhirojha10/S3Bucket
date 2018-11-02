f3ac2aaf74ff6653838beebb13283a31a5a50625
Sudhir Ojha <sudhirojha@panaru.chaudhary>
2018-11-02 13:09:13 +0530

    First commit s3bucket example

---
+++ src/main/java/com/s3bucket/S3Bucket/S3BucketApplication.java
@@ -0,0 +1,12 @@
+package com.s3bucket.S3Bucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3BucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3BucketApplication.class, args);
+	}
+}
