302efefeca3c35428dda6f656d208fb5d05c1813
Sudhir Ojha <sudhir.kumar@oodlestechnologies.com>
2019-01-25 17:09:31 +0530

    Update BootStrap.java

--- src/main/java/com/s3bucket/S3Bucket/BootStrap.java
+++ src/main/java/com/s3bucket/S3Bucket/BootStrap.java
@@ -1,1 +1,16 @@
 
+@Component
+public class BootStrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static Logger logger = Logger.getLogger(BootStrap.class);
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent arg0) {
+        methodName();
+        addInitialMarketData();
+
+    }
+    void methodName() {
+      logger.info("Called methodName()");
+    }
+}
